fe0b17fdd34d0065b8abe33221ad2a323d0d8089
Coder-fox-bd <ahrabbi121@gmail.com>
2022-04-19 01:08:10 +0600

    Update cart-js.blade.php

--- resources/views/frontend/extra/cart-js.blade.php
+++ resources/views/frontend/extra/cart-js.blade.php
@@ -1015,7 +1015,6 @@
                             'data': {'rec':item_id,'_token':"{{csrf_token()}}"},
 
                             'success': function (data) {
-                                // toastr.success('Item added to cart');
                                 //tmp = data;
 
                               //  console.log("Processing : "+data);
